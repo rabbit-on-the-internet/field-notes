664b1e6818f4b5e07d2147929724856daf88cc00
Rabbit <hey.caramargot@gmail.com>
2024-10-12 11:49:36 -0400

    Add gatsby to neocities fr this time

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>u did it</h1> 
+      <h2>queen of developing and code</h2>
+      </main>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Home Page</title>
